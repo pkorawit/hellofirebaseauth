51e53b25ce1f85840d18c2cb738ac59e417b0f33
Korawit Prutsachainimmit <korawit.p@gmail.com>
2020-10-12 11:42:06 +0700

    Init with Firebase auth

---
+++ firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDEe7zcyacSqJWNhMcxbfkdnwxkd1XF1sc",
+    authDomain: "plearnhub-be749.firebaseapp.com",
+    databaseURL: "https://plearnhub-be749.firebaseio.com",
+    projectId: "plearnhub-be749",
+    storageBucket: "plearnhub-be749.appspot.com",
+    messagingSenderId: "429650509455",
+    appId: "1:429650509455:web:adadc8b6215fe21011fafa",
+    measurementId: "G-HG7G7ZEFPC"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
